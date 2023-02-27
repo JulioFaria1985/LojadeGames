1fd578322dfa03999e08cfa39eb3aa1be500d304
jufaria <julio.faria@mercadolivre.com>
2023-02-27 09:55:47 -0300

    primeiro commit

---
+++ src/main/java/com/lojadegames/lojadegames/model/Categoria.java
@@ -0,0 +1,34 @@
+package com.lojadegames.lojadegames.model;
+
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+    @Table(name = "tb_categorias")
+    public class Categoria {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+
+        @NotNull(message = "O Atributo Descrição é obrigatório")
+        private String descricao;
+
+        public Long getId() {
+            return this.id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+        public String getDescricao() {
+            return this.descricao;
+        }
+
+        public void setDescricao(String descricao) {
+            this.descricao = descricao;
+        }
+
+    }
